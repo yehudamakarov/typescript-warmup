412dc30def32266fbf0604e27b5fd91847d0adb9
Yehuda Makarov <yehudamakarov@gmail.com>
2018-11-20 00:19:37 -0500

    Now scraping a Chumash object
    still need to add Rashi

--- src/scraper/scraper.ts
+++ src/scraper/scraper.ts
@@ -2,6 +2,34 @@ import * as $ from "cheerio";
 import moment from "moment";
 import rp from "request-promise";
 
+// export a function that takes args. Subjects and start/end date to fill up the
+// DB with.
+
+interface IHebrewContentItem {
+    [index: string]: string;
+}
+
+interface IEnglishContentItem {
+    [index: string]: string;
+}
+
+type HebrewContent = IHebrewContentItem[];
+type EnglishContent = IEnglishContentItem[];
+
+interface IChumashObject {
+    learnOnDate: Date;
+    dayOfTheWeek: string;
+    hebrewContent: HebrewContent;
+    englishContent: EnglishContent;
+    amountOfPesukim: number;
+    aliyah: number;
+    mmddyyyy: string;
+    // parshaNameHebrew: string;
+    parshaNameEnglish: string;
+    // seferHebrew: string;
+    // seferEnglish: string;
+}
+
 const subjectsForUrl = {
     chumash: "torahreading",
     tanya: "tanya",
@@ -9,11 +37,49 @@ const subjectsForUrl = {
 };
 
 // "11/11/2018"
-const momentDate = moment().format("MM/DD/YYYY");
+const now = moment();
+const learnOnDate: Date = new Date(now.year(), now.month(), now.date());
+const dayOfTheWeek: string = now.format("dddd");
+const aliyah = now.day() + 1;
+const mmddyyyy = now.format("MM/DD/YYYY");
 
-const url = `https://www.chabad.org/dailystudy/${subjectsForUrl.chumash}.asp?tdate=${momentDate}`;
+const url = `https://www.chabad.org/dailystudy/${subjectsForUrl.chumash}.asp?tdate=${mmddyyyy}`;
 
 rp(url).then((html) => {
-    // tslint:disable-next-line:no-debugger
-    debugger;
+    const doc = $.load(html);
+
+    const parshaNameParts = doc(".article-header__subtitle")[0].childNodes[0].nodeValue.split(" ");
+    parshaNameParts.shift();
+    const parshaNameEnglish = parshaNameParts.join("").trim();
+
+    const pesukimRows = doc(".Co_Verse");
+    const amountOfPesukim = pesukimRows.length;
+    const hebrewTdNodes = doc(".Co_Verse td:nth-child(3)");
+    const englishTdNodes = doc(".Co_Verse td:nth-child(1)");
+
+    const hebrewContent: HebrewContent = [];
+    const englishContent: EnglishContent = [];
+
+    for (let i = 0; i < amountOfPesukim; i++) {
+        hebrewContent.push({
+            [hebrewTdNodes[i].childNodes[0].childNodes[0].nodeValue]:
+                hebrewTdNodes[i].childNodes[1].childNodes[0].nodeValue,
+        });
+        englishContent.push({
+            [englishTdNodes[i].childNodes[1].childNodes[0].nodeValue]:
+                englishTdNodes[i].childNodes[2].childNodes[0].nodeValue,
+        });
+
+    }
+
+    const chumashObjectForDb: IChumashObject = {
+        aliyah,
+        amountOfPesukim,
+        dayOfTheWeek,
+        englishContent,
+        hebrewContent,
+        learnOnDate,
+        mmddyyyy,
+        parshaNameEnglish,
+    };
 });
